bdc70bf2e8f2d4e8202e7bc6be7bff027399001e
Akash <ferdous.hasan01@northsouth.edu>
2024-05-24 20:51:48 +0600

    First Commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { config } from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+      method: ["GET", "POST", "PUT" , "DELETE"],
+      credentials: true,
+    })
+  );
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/",
+    })
+  );
+
+dbConnection();
+
+export default app;
